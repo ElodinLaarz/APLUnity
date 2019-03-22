f08da2730ef50c4513a080a6d2b5bb83608c4512
ElodinLaarz <48812321+ElodinLaarz@users.noreply.github.com>
2019-03-22 15:55:14 -0700

    Trying not to Delete Sharat's file

--- APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
+++ APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
@@ -6,7 +6,7 @@ public class AlbertandCalebandSharatDuelinC# : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+    int caleb = 1;
 	}
 	
 	// Update is called once per frame

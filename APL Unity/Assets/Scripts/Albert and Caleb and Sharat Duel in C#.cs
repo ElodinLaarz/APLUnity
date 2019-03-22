7191ac71672de1f825b4aa1dff60e77e08d355d2
ElodinLaarz <48812321+ElodinLaarz@users.noreply.github.com>
2019-03-22 16:09:48 -0700

    meep

--- APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
+++ APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
@@ -9,7 +9,11 @@ public class AlbertandCalebandSharatDuelinC# : MonoBehaviour {
 	void Start () {
     int caleb = 1;
 	}
+<<<<<<< HEAD
 	//Caleb Change this is my file poo
+=======
+	//Caleb Change this is my file
+>>>>>>> parent of b8cc788... Albert conflict
 	//fuck this language
 	// Update is called once per frame
 	void Update () {

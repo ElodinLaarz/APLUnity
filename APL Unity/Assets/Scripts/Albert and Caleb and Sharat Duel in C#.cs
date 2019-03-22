27fd258e38e7a921c8c3914de28b120f639bafcb
ElodinLaarz <48812321+ElodinLaarz@users.noreply.github.com>
2019-03-22 16:17:06 -0700

    revert different

--- APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
+++ APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
@@ -10,11 +10,15 @@ public class AlbertandCalebandSharatDuelinC# : MonoBehaviour {
     int caleb = 1;
 	}
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> parent of b8cc788... Albert conflict
 	//Caleb Change this is my file
 	//fuck this language
+=======
+	#this is my file
+>>>>>>> parent of 06ac4f5... Editing same line
 	// Update is called once per frame
 	void Update () {
 		

2b3e189dc2b3b52672f93151306afc7d7b3b1aac
ElodinLaarz <48812321+ElodinLaarz@users.noreply.github.com>
2019-03-22 16:36:34 -0700

    Albert and Caleb and Sharat Duel in C#.cs

--- APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
+++ APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class AlbertandCalebandSharatDuelinC# : MonoBehaviour {
 
 	// Use this for initialization
-	//This is Sharat's edits
 	void Start () {
-    int caleb = 1;
+		
 	}
-    //Reverting
-	//fuck this language
+	
 	// Update is called once per frame
 	void Update () {
 		

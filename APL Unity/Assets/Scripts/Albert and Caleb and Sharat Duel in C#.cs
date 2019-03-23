4f4449280fb597ffe88b5fd4b37f9fe7d4d1598b
ElodinLaarz <48812321+ElodinLaarz@users.noreply.github.com>
2019-03-23 00:06:27 -0700

    Sample Dungeon Collision Scene
    Created a sample dungeon scene with a controlable character for anyone who is interested in messing around.
    
    A simple next step would be to update the collision boxes of the dungeon walls to allow the character to fit up into the smaller regions of the room. (Remove some of the collision boxes from the upperside of dungeon walls.)

--- APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
+++ APL Unity/Assets/Scripts/Albert and Caleb and Sharat Duel in C#.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AlbertandCalebandSharatDuelinC# : MonoBehaviour {
+public class AlbertandCalebandSharatDuelinCSharp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {

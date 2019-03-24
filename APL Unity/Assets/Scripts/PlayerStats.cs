f1f66cc031055de1d0ddd7cea6771acaa5657635
ElodinLaarz <48812321+ElodinLaarz@users.noreply.github.com>
2019-03-24 11:47:33 -0700

    Adding Player Stats and a TMP overlay

---
+++ APL Unity/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    int health = 100;
+
+    public TextMeshProUGUI hpText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpText.text = health.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Damage(int d)
+    {
+
+    }
+}

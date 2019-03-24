d3639ab68ed39e7e659849f997dca3aa0fc64bbc
ElodinLaarz <48812321+ElodinLaarz@users.noreply.github.com>
2019-03-23 19:01:08 -0700

    Assorted Edits

---
+++ APL Unity/Assets/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}

aaa94e45bee16c80f1078dd149b781e9004a3960
ElodinLaarz <48812321+ElodinLaarz@users.noreply.github.com>
2019-03-24 12:07:45 -0700

    Might not have committed Status Update...

--- APL Unity/Assets/Scripts/Enemy.cs
+++ APL Unity/Assets/Scripts/Enemy.cs
@@ -6,6 +6,8 @@ public class Enemy : MonoBehaviour
 {
     public int health = 100;
 
+    public int damage = 10;
+
     //public GameObject deathEffect;
 
     public void TakeDamage(int damage)
@@ -17,6 +19,16 @@ public class Enemy : MonoBehaviour
             Die();
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerStats playerStats = collision.collider.GetComponent<PlayerStats>();
+        if(playerStats != null)
+        {
+            playerStats.Damage(damage);
+        }
+    }
+
 
     void Die()
     {
--- APL Unity/Assets/Scripts/PlayerStats.cs
+++ APL Unity/Assets/Scripts/PlayerStats.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    int health = 100;
+    public int health = 100;
 
-    public TextMeshProUGUI hpText;
+    //public GameObject player;
+
+
+     //public TextMeshProUGUI hpText;
     // Start is called before the first frame update
     void Start()
     {
-        hpText.text = health.ToString();
+    //    hpText.text = health.ToString();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    // Just testing to see if the HP was being updated in the GUI
+    //    Damage(1);
     }
 
-    void Damage(int d)
+    public void Damage(int d)
     {
-
+        health -= d;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }

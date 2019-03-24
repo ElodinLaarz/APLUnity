2bfe5e6a57576295a254e25ee7eba15a6af05c68
ElodinLaarz <48812321+ElodinLaarz@users.noreply.github.com>
2019-03-23 19:07:22 -0700

    Weaponized Penis

---
+++ APL Unity/Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb.velocity = transform.right * speed;
+	}
+
+    void OnTriggerEnter2D()
+    {
+
+    }
+}

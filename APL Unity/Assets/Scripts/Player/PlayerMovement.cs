381695121605f970071adaad97c1207a05a3d663
SwifferCat <34951593+SwifferCat@users.noreply.github.com>
2019-03-27 11:00:35 -0700

    Added a Dash (bound to z)

--- APL Unity/Assets/Scripts/Player/PlayerMovement.cs
+++ APL Unity/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,17 @@ public class PlayerMovement : MonoBehaviour
     // Normal Movements Variables
     public float walkSpeed = 0;
     private float curSpeed = 0;
+    private bool inDash = false;
+    private float timeInDash;
+    private Vector2 dashDirection;
+    private Vector2 moveVelocity;
+    private Vector2 dashVelocity;
+    public float dashSpeed;
+    public float dashBuildUp;
+    public float dashSlowDown;
+    public float fullSpeedDuration;
+
+
 
     //Allows us to add the collider of our player to the movement script.
     public Animator animator;
@@ -38,9 +49,44 @@ public class PlayerMovement : MonoBehaviour
         // maxSpeed = curSpeed;
 
         // Move senteces
-        rb.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
-                                             Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+        moveVelocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+                                         Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+
+        if (Input.GetKey("z") && !inDash)
+        {
+            dashDirection = rb.velocity;
+            dashDirection.Normalize();
+            inDash = true;
+            timeInDash = 0;
+        }
+
+        if (inDash)
+        {
+            if (timeInDash < dashBuildUp)
+            {
+                dashVelocity = new Vector2(dashSpeed * (timeInDash / dashBuildUp) * dashDirection.x, dashSpeed * (timeInDash / dashBuildUp) * dashDirection.y);
+            }
+            else if (timeInDash < fullSpeedDuration)
+            {
+                dashVelocity = new Vector2(dashSpeed * dashDirection.x, dashSpeed * dashDirection.y);
+                Debug.Log(dashVelocity);
+            }
+            else if (timeInDash < fullSpeedDuration + dashSlowDown)
+            {
+                dashVelocity = new Vector2(dashSpeed * ((fullSpeedDuration + dashSlowDown - timeInDash) / dashSlowDown) * dashDirection.x, dashSpeed * ((fullSpeedDuration + dashSlowDown - timeInDash) / dashSlowDown) * dashDirection.y);
+            }
+            else
+            {
+                inDash = false;
+                dashVelocity = new Vector2(0, 0);
+                Debug.Log("turned off");
+            }
+            timeInDash = timeInDash + Time.deltaTime;
+        }
+
+        rb.velocity = dashVelocity + moveVelocity;
         animator.SetFloat("Xspeed", rb.velocity.x);
         animator.SetFloat("Yspeed", rb.velocity.y);
+
     }
 }
